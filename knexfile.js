29d73fcf7b2f36d7e2c934a51a54ac5c3eaf3051
Michael Hottman <hottmanmichael@gmail.com>
2016-09-26 15:12:09 -0600

    changed knex setup

--- knexfile.js
+++ knexfile.js
@@ -14,7 +14,14 @@ module.exports = {
 
     production: {
         client: 'pg',
-        connection: process.env.RDS_HOSTNAME
+        connection: {
+            host: process.env.RDS_HOSTNAME,
+            port: process.env.RDS_PORT,
+            database: process.env.RDS_DB_NAME,
+            user: process.env.RDS_USERNAME,
+            password: process.env.RDS_PASSWORD
+        },
+        migrations: 'migrations'
     }
 
 };
